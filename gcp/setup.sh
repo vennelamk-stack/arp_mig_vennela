c9d7b54bc3001304458825e5698135ab3f73c563
vennelamk-stack <vennelamk@google.com>
2026-07-16 11:42:36 +0530

    Update setup.sh

--- gcp/setup.sh
+++ gcp/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-# Copyright 2026 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#      https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -31,10 +31,14 @@ trap _upload_install_log EXIT
 pushd $SCRIPT_PATH >/dev/null
 
 while :; do
-  case $1 in
-    -s|--settings) shift SETTING_FILE=$1 ;;
-    *) break
-  esac
+    case $1 in
+  -s|--settings)
+      shift
+      SETTING_FILE=$1
+      ;;
+  *)
+      break
+    esac
   shift
 done
 
@@ -42,22 +46,24 @@ NAME=$(git config -f $SETTING_FILE config.name)
 PROJECT_ID=$(gcloud config get-value project 2> /dev/null)
 PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="csv(projectNumber)" | tail -n 1)
 USER_EMAIL=$(gcloud config get-value account 2> /dev/null)
+
 APP_CONFIG_FILE=$(eval echo $(git config -f $SETTING_FILE config.config-file))
 REPOSITORY=$(eval echo $(git config -f $SETTING_FILE repository.name))
 IMAGE_NAME=$(eval echo $(git config -f $SETTING_FILE repository.image))
 REPOSITORY_LOCATION=$(git config -f $SETTING_FILE repository.location)
 
-# Cloud Run Job Vars
+# Initialize SERVICE_ACCOUNT first so RUN_SA can safely read it as a default fallback
+SERVICE_ACCOUNT=$PROJECT_NUMBER-compute@developer.gserviceaccount.com
+GCS_BASE_PATH=gs://$PROJECT_ID/$NAME
+
+# New Cloud Run Job variables (replacing legacy VM/Pub-Sub parameters)
 JOB_NAME=$(eval echo $(git config -f $SETTING_FILE cloud-run.name))
 JOB_REGION=$(git config -f $SETTING_FILE cloud-run.region)
 JOB_CPU=$(git config -f $SETTING_FILE cloud-run.cpu)
 JOB_MEMORY=$(git config -f $SETTING_FILE cloud-run.memory)
 JOB_TIMEOUT=$(git config -f $SETTING_FILE cloud-run.task-timeout)
 RUN_SA=$(eval echo $(git config -f $SETTING_FILE cloud-run.service-account))
-SERVICE_ACCOUNT=$PROJECT_NUMBER-compute@developer.gserviceaccount.com
-RUN_SA=${RUN_SA:-$SERVICE_ACCOUNT}
-
-GCS_BASE_PATH=gs://$PROJECT_ID/$NAME
+RUN_SA=${RUN_SA:-$SERVICE_ACCOUNT} # default to the compute SA
 
 check_billing() {
   BILLING_ENABLED=$(gcloud beta billing projects describe $PROJECT_ID --format="csv(billingEnabled)" | tail -n 1)
@@ -95,8 +101,10 @@ deploy_files() {
     echo "Creating GCS bucket gs://$PROJECT_ID"
     gcloud storage buckets create --uniform-bucket-level-access gs://$PROJECT_ID
   fi
+
   echo "Removing existing files at $GCS_BASE_PATH"
   gcloud storage rm --recursive $GCS_BASE_PATH/
+
   copy_application_scripts
   copy_application_config
   copy_googleads_config
@@ -105,7 +113,7 @@ deploy_files() {
 enable_apis() {
   echo "Enabling APIs"
   gcloud services enable bigquery.googleapis.com
-  gcloud services enable compute.googleapis.com
+  gcloud services enable compute.googleapis.com # still used for the AR/Cloud Build base; safe to keep
   gcloud services enable containerregistry.googleapis.com
   gcloud services enable run.googleapis.com
   gcloud services enable cloudresourcemanager.googleapis.com
@@ -119,9 +127,10 @@ create_registry() {
   REPO_EXISTS=$(gcloud artifacts repositories list --location=$REPOSITORY_LOCATION --filter="REPOSITORY=projects/'$PROJECT_ID'/locations/'$REPOSITORY_LOCATION'/repositories/'"$REPOSITORY"'" --format="value(REPOSITORY)" 2>/dev/null)
   if [[ ! -n $REPO_EXISTS ]]; then
     echo "Creating a repository in Artifact Registry"
+    # repo doesn't exist, creating
     gcloud artifacts repositories create ${REPOSITORY} \
-      --repository-format=docker \
-      --location=$REPOSITORY_LOCATION
+        --repository-format=docker \
+        --location=$REPOSITORY_LOCATION
     exitcode=$?
     if [ $exitcode -ne 0 ]; then
       echo -e "${RED}[ ! ] Please upgrade Cloud SDK to the latest version: gcloud components update"
@@ -134,7 +143,14 @@ build_docker_image() {
   gcloud builds submit --config=cloudbuild.yaml --substitutions=_REPOSITORY="docker",_IMAGE="$IMAGE_NAME",_REPOSITORY_LOCATION="$REPOSITORY_LOCATION" ./..
 }
 
+build_docker_image_gcr() {
+  # NOTE: it's an alternative to build_docker_image if you want to use GCR instead of AR
+  echo "Building and pushing Docker image to Container Registry"
+  gcloud builds submit --config=cloudbuild-gcr.yaml --substitutions=_IMAGE="$IMAGE_NAME" ./..
+}
+
 set_iam_permissions() {
+  # Reduced roles set (includes BigQuery access, drops legacy VM/Pub-Sub permissions)
   required_roles="bigquery.dataEditor bigquery.jobUser storage.objectAdmin artifactregistry.reader run.invoker iam.serviceAccountUser"
   echo "Setting up IAM permissions"
   for role in $required_roles; do
@@ -156,6 +172,7 @@ deploy_job() {
     sa_flag="--service-account=$RUN_SA"
   fi
 
+  # Mount GCS configurations directly to container /config mount path
   gcloud run jobs deploy $JOB_NAME \
     --image=$IMAGE \
     --region=$JOB_REGION \
@@ -169,9 +186,11 @@ deploy_job() {
 
 deploy_public_index() {
   echo 'Deploying index.html to GCS'
+
   if ! gcloud storage ls gs://$PROJECT_ID-public > /dev/null 2> /dev/null; then
     gcloud storage buckets create --uniform-bucket-level-access gs://$PROJECT_ID-public
   fi
+
   gcloud storage buckets add-iam-policy-binding gs://${PROJECT_ID}-public --member=allUsers --role=roles/storage.objectViewer 2> /dev/null
   exitcode=$?
   if [ $exitcode -ne 0 ]; then
@@ -189,6 +208,7 @@ start() {
   dashboard_url=$(./../app/scripts/create_dashboard.sh -L --config ./../app/$APP_CONFIG_FILE)
 
   echo 'Executing Cloud Run Job '$JOB_NAME
+  # async: don't block install on a large first run; user watches check_logs.sh.
   gcloud run jobs execute $JOB_NAME --region=$JOB_REGION
 
   # Check if there is a public bucket and index.html and echo the url
@@ -196,6 +216,7 @@ start() {
   local ARP_GOOGLE_GROUP="https://groups.google.com/g/app-reporting-pack-readers-external"
   echo -e "${CYAN}[ * ] Please join Google group to get access to the dashboard - ${GREEN}${ARP_GOOGLE_GROUP}${NC}"
   STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" $PUBLIC_URL)
+
   if [[ $STATUS_CODE -eq 200 ]]; then
     echo -e "${CYAN}[ * ] To access your new dashboard, click this link - ${GREEN}${PUBLIC_URL}${NC}"
   else
@@ -223,7 +244,7 @@ schedule_run() {
   local SCHEDULE_TZ=$(git config -f $SETTING_FILE scheduler.schedule-timezone)
   SCHEDULE_TZ=${SCHEDULE_TZ:-"Etc/UTC"}
 
-  delete_schedule
+  delete_schedule # existing existence-checked delete, reused unchanged
 
   echo 'Scheduling HTTP job to run Cloud Run Job '$JOB_NAME
   gcloud scheduler jobs create http $SCHED_NAME \
@@ -238,6 +259,7 @@ schedule_run() {
 delete_schedule() {
   JOB_NAME=$(eval echo $(git config -f $SETTING_FILE scheduler.name))
   REGION=$(git config -f $SETTING_FILE scheduler.region)
+
   JOB_EXISTS=$(gcloud scheduler jobs list --location=$REGION --format="value(ID)" --filter="ID=projects/'$PROJECT_ID'/locations/'$REGION'/jobs/'$JOB_NAME'" 2>/dev/null)
   if [[ -n $JOB_EXISTS ]]; then
     echo 'Deleting Cloud Scheduler job '$JOB_NAME
@@ -249,10 +271,11 @@ check_owners() {
   local project_admins=$(gcloud projects get-iam-policy $PROJECT_ID \
     --flatten="bindings" \
     --filter="bindings.role=roles/owner OR bindings.role=roles/admin" \
-    --format="value(bindings.members[])" )
+    --format="value(bindings.members[])"
+  )
   if [[ ! $project_admins =~ $USER_EMAIL ]]; then
-    echo "User $USER_EMAIL does not have admin / owner rights to project $PROJECT_ID"
-    exit
+      echo "User $USER_EMAIL does not have admin / owner rights to project $PROJECT_ID"
+      exit
   fi
 }
 
@@ -270,13 +293,13 @@ deploy_all() {
 
 migrate_to_cloud_run() {
   echo "Migrating legacy Cloud Function + Pub/Sub + VM stack to Cloud Run Jobs"
-  local CF_REGION="europe-west1"
   local CF_NAME="create-${NAME}-vm"
   local OLD_TOPIC="run-${NAME}"
+  local CF_REGION="europe-west1"
   local SCHED_NAME=$(eval echo $(git config -f $SETTING_FILE scheduler.name))
   local SCHED_REGION=$(git config -f $SETTING_FILE scheduler.region)
 
-  # 1) delete the legacy pubsub-type scheduler job ONLY
+  # 1) delete the legacy pubsub-type scheduler job ONLY (http = already migrated)
   if gcloud scheduler jobs describe "$SCHED_NAME" --location="$SCHED_REGION" >/dev/null 2>&1; then
     local TARGET=$(gcloud scheduler jobs describe "$SCHED_NAME" --location="$SCHED_REGION" \
       --format='value(pubsubTarget.topicName)')
@@ -296,14 +319,16 @@ migrate_to_cloud_run() {
     gcloud pubsub topics delete "$OLD_TOPIC" --quiet
   fi
 
-  # 4) stand up the new stack
+  # 4) stand up the new stack (idempotent: deploy_job / schedule_run create-or-update)
   set_iam_permissions
   build_docker_image
   deploy_job
   schedule_run
-  echo "Migration to Cloud Run Jobs complete. Old roles (compute.admin, pubsub.*) can be revoked with: ./gcp/setup.sh tighten_iam"
+  echo "Migration to Cloud Run Jobs complete. Old roles (compute.admin, pubsub.*) can be"
+  echo "revoked with: ./gcp/setup.sh tighten_iam"
 }
 
+# OPT-IN, never called automatically: revoke roles the old model needed.
 tighten_iam() {
   for role in compute.admin pubsub.publisher monitoring.editor logging.logWriter; do
     gcloud projects remove-iam-policy-binding $PROJECT_ID \

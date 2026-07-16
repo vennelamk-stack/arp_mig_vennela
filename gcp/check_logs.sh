aeb0843b6c4615d0a3ecab6dff491f42c01c26dc
vennelamk-stack <vennelamk@google.com>
2026-07-16 11:56:45 +0530

    Update check_logs.sh

--- gcp/check_logs.sh
+++ gcp/check_logs.sh
@@ -18,7 +18,8 @@ SETTING_FILE="${SCRIPT_PATH}/settings.ini"
 NAME=$(git config -f $SETTING_FILE config.name)
 PROJECT_ID=$(gcloud config get-value project 2> /dev/null)
 
+# New Cloud Run Job query structure replacing the legacy VM logName query
 QUERY="resource.type%3D%22cloud_run_job%22%20resource.labels.job_name%3D%22${NAME}%22"
+
 echo "Click the link below to get access to application logs for the last 7 days"
 echo "https://console.cloud.google.com/logs/query;query=${QUERY};duration=P7D?project=${PROJECT_ID}"
-
